0af9234425167da55b15c63a067f3eb21e3fe3e0
Aaron Wiggins <apw7177@gmail.com>
2015-06-29 22:58:47 -0400

    login with git and fitbit but fitbit doesn't redirect back to my site

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,9 +238,14 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'  
+  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  # for github
   config.omniauth :github, ENV['CLASS_GITHUB_ID'], ENV['CLASS_GITHUB_SECRET']
-
+  # for fitbit
+  # Rails.application.config.middleware.use OmniAuth::Builder do
+  #   provider :fitbit, ENV['FITBIT_KEY'], ENV['FITBIT_SECRET']
+  # end
+  config.omniauth :fitbit, ENV["FITBIT_KEY"], ENV["FITBIT_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
